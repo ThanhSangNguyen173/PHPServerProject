9bfb6c6c2c8a060c9d059a865669ac86c7d823ad
ThanhSangNguyen <thanhsang.nguyen.vtp@gmail.com>
2022-05-13 13:33:27 +0700

    [add] API put, delete products

--- database/migrations/2022_05_13_042026_create_products_table.php
+++ database/migrations/2022_05_13_042026_create_products_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->integer('price');
             $table->boolean('status');
+            $table->string('imgurl');
         });
     }
 
