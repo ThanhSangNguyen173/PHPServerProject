3361ad037db48172ce75ac98b6e365093bf2a520
GitRepo <nguyenduchung.lhu@gmail.com>
2022-05-16 10:36:23 +0700

    [API]dang nhap

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\Users as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
 class Users extends Model
 {
     use HasFactory;
@@ -17,4 +22,25 @@ class Users extends Model
         'email',
         'DOB',
     ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'api_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
 }
