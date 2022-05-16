aed776aa6ed28f02dde2a4df2c979c03e935af72
GitRepo <nguyenduchung.lhu@gmail.com>
2022-05-16 10:36:23 +0700

    [API]dang nhap

--- database/migrations/2022_05_13_071046_create_users_table.php
+++ database/migrations/2022_05_13_071046_create_users_table.php
@@ -16,10 +16,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('full_name');
+            $table->date('DOB');
             $table->string('username');
             $table->string('password');
-            $table->string('email');
-            $table->date('DOB');
+            $table->string('email')->unique();
+            $table->timestamps('email_verified_at')->nullable();
+            $table->string('api_token');
             $table->timestamps();
         });
     }
