cacf2d71879256edf140ddee62a876a7a468f1a0
dangqthuan15 <dangqthuan15@gmail.com>
2022-05-20 09:03:59 +0700

    edit migrations products

--- database/migrations/2022_05_13_042026_create_products_table.php
+++ database/migrations/2022_05_13_042026_create_products_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('price');
-            $table->boolean('status');
-            $table->string('imgurl');
+            $table->string('price');
+            $table->string('isAvailable');
+            $table->string('imgUrl');
         });
     }
 
