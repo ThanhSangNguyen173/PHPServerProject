1ef743894b39a16c09bf2e793b85b4a874551218
ThanhSangNguyen <thanhsang.nguyen.vtp@gmail.com>
2022-05-23 09:32:00 +0700

    edit Products model

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -14,7 +14,7 @@ class Products extends Model
     protected $fillable = [
         'name',
         'price',
-        'status', 
-        'imgurl',
+        'isAvailable', 
+        'imgUrl',
     ];
 }
